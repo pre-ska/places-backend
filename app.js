ecf49b97029be7d6e520177915a2cb4045034617
wolfwar <wolfwar@outlook.com>
2020-04-22 11:46:39 +0200

    error handler 1

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const placesRoutes = require("./routes/places-routes");
 
 const app = express();
 
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes); // => api/places/...
+
+// ako midleware ima 4 argumenta, express je tretira kao error handleing midleware function
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    // ako je response vec poslan
+    return next(error);
+  }
+
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || "unknown error" });
+});
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,71 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET request in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire state building",
+    description: "bla bla bla",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire state building",
+    description: "bla bla bla",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u2",
+  },
+];
+
+// ovaj path "/" se nastavlja na filter u app.js "/api/places" -> za ovaj router
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => p.id === placeId);
+
+  // if (!place) {
+  //   return res
+  //     .status(404)
+  //     .json({ message: "Could not find a place with that ID" });
+  // }
+  // zamjenio sa error handleing u app.js 6-10
+
+  if (!place) {
+    const error = new Error("Could not find a place with that ID");
+    error.code = 404;
+    throw error; // moze samo za sihroni kod...za asihroni kod mora ici next(error)
+    //next ako ima argument  znaci da je error handleing middleware
+    // throw error NE ZAHTJEVA return... kod se prekida svejedno
+  }
+
+  res.json({ place: place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find(u => u.creator === userId);
+
+  // if (!place) {
+  //   return res
+  //     .status(404)
+  //     .json({ message: "Could not find a place with that user ID" });
+  // }
+  //zamjenio !!!!!!! 6-10
+  if (!place) {
+    const error = new Error("Could not find a place with that user ID");
+    error.code = 404;
+    return next(error); // verzija sa next ...kao primjer...mogao je i throw error
+  }
+  res.json({ place: place });
 });
 
 module.exports = router;
